d480f2e72bd1b046265665582f6460315ffe15ea
Jasmine <jasminee@gmail.com>
2023-05-21 18:17:06 -0400

    removed build folder

--- client/index.js
+++ client/index.js
@@ -1,25 +1,25 @@
 
-// import React from 'react';
-// import { render } from 'react-dom';
-// import App from './components/App';
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
 
-// // uncomment so that webpack can bundle styles
-// import styles from './scss/application.scss';
+// uncomment so that webpack can bundle styles
+import styles from './scss/application.scss';
 
-// render(
-//     <App />,
-//     document.getElementById('root')
-// );
-
-import React from "react"
-import ReactDOM from "react-dom"
-import { BrowserRouter as Router } from "react-router-dom"
-
-import App from './components/App'
-
-ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
+render(
+    <App />,
     document.getElementById('root')
-)
+);
+
+// import React from "react"
+// import ReactDOM from "react-dom"
+// import { BrowserRouter as Router } from "react-router-dom"
+
+// import App from './components/App'
+
+// ReactDOM.render(
+//     <Router>
+//         <App />
+//     </Router>,
+//     document.getElementById('root')
+// )
