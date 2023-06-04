bd4ea6171c5e969c2b150287f6c9dd0cee068b34
Jasmine <jasminee@gmail.com>
2023-06-04 17:12:40 -0400

    formatting

--- client/components/ResultsContainer.js
+++ client/components/ResultsContainer.js
@@ -2,31 +2,31 @@ import React, { Component } from "react";
 import Result from "./Result.js";
 
 class ResultsContainer extends Component {
-    render() {
-        let resultArr = this.props.speciesList.map((x, i) => {
-            return (
-                <Result
-                    handleClick={this.props.handleClick}
-                    key={`result-${i}`}
-                    count={x.count}
-                    name={x.name}
-                    url={x.medium_url}
-                    speciesID={x.id}
-                    common={x.preferred_common_name}
-                    nature_option={this.props.nature_option}
-                    handleClick={this.props.handleClick}
-                />
-            );
-        });
+  render() {
+    let resultArr = this.props.speciesList.map((x, i) => {
+      return (
+        <Result
+          handleClick={this.props.handleClick}
+          key={`result-${i}`}
+          count={x.count}
+          name={x.name}
+          url={x.medium_url}
+          speciesID={x.id}
+          common={x.preferred_common_name}
+          nature_option={this.props.nature_option}
+          handleClick={this.props.handleClick}
+        />
+      );
+    });
 
-        return (
-            <div>
-                <h1 className='seenSince'>{`${this.props.headerLocation}  ${this.props.date}`}</h1>
+    return (
+      <div>
+        <h1 className='seenSince'>{`${this.props.headerLocation}  ${this.props.date}`}</h1>
 
-                <div className='resultsContainer'>{resultArr}</div>
-            </div>
-        );
-    }
+        <div className='resultsContainer'>{resultArr}</div>
+      </div>
+    );
+  }
 }
 
 export default ResultsContainer;
--- client/index.js
+++ client/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App';
-import styles from './scss/application.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import styles from "./scss/application.scss";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
