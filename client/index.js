e31abd8995de049355a8be3682f41c55f9ad3f7a
Jasmine <jasminee@gmail.com>
2023-06-04 17:00:15 -0400

    formatting

--- client/index.js
+++ client/index.js
@@ -1,17 +1,11 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './components/App';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-import React from 'react';
-import { render } from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './components/App';
-
-// uncomment so that webpack can bundle styles
 import styles from './scss/application.scss';
 
-render(
-    <App />,
-    document.getElementById('root')
-);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+)
