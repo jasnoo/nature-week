a7e33f44585007744a583441b62fb7100863c517
Jasmine <jasminee@gmail.com>
2023-06-20 12:35:36 -0400

    temp removing route behavior

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 function Home() {
-    return <h1>Home</h1>
+    // return <h1>Home</h1>
+    return null
 }
 
 export default Home
--- client/components/Result.js
+++ client/components/Result.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import ResultFavorite from "./ResultFavorite";
-import React, { Component } from "react";
-import ResultFavorite from "./ResultFavorite";
 
 class Result extends Component {
   constructor(props) {
@@ -30,20 +28,7 @@ class Result extends Component {
       this.getUpdatedFavorite();
     }
   }
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState !== this.state) {
-      this.getUpdatedFavorite();
-    }
-  }
 
-  render() {
-    return (
-      <div
-        className='result'
-        onClick={this.props.handleClick}
-        isFavorite={this.state.isFavorite}
-      >
-        <h2>{this.props.common}</h2>
   render() {
     return (
       <div
@@ -56,11 +41,7 @@ class Result extends Component {
         <h3>{this.props.name}</h3>
         <img src={this.props.url}></img>
         <h4>Seen {this.props.count} this week </h4>
-        <h3>{this.props.name}</h3>
-        <img src={this.props.url}></img>
-        <h4>Seen {this.props.count} this week </h4>
 
-        {/* <div url={this.props.url}
         {/* <div url={this.props.url}
                     nature_option={this.props.nature_option}
                     speciesID={this.props.speciesID}
@@ -87,4 +68,3 @@ class Result extends Component {
 }
 
 export default Result;
-
