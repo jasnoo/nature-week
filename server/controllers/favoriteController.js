c540f07991e44c0813748b2037202dc617bc0b22
Jasmine <jasminee@gmail.com>
2023-07-05 16:55:57 -0400

    removed unused lib

--- server/controllers/favoriteController.js
+++ server/controllers/favoriteController.js
@@ -1,7 +1,5 @@
 
-const path = require("path");
 const { User } = require("../models/user");
-const { isFloat32Array } = require("util/types");
 
 const favoriteController = {};
 
@@ -55,40 +53,6 @@ favoriteController.getAllFavorites = (req, res, next) => {
     next();
   }
 }
-
-// favoriteController.getFavoriteData = (req, res, next) => {
-
-//   if (res.locals.favorites.favorites[0] !== undefined) {
-//     const favString = res.locals.favorites.favorites.join("%2C")
-//     try {
-//       fetch(`https://api.inaturalist.org/v1/taxa/${favString}`)
-//         .then((response) => response.json())
-//         .then((data) => {
-//           let favoritesInfo = data.results.map(x => {
-//             return {
-//               id: x.id,
-//               preferred_common_name: x.preferred_common_name,
-//               name: x.name,
-//               medium_url: x.default_photo.medium_url,
-//               nature_option: x.iconic_taxon_name,
-//             }
-
-//           })
-//           res.locals.favorites = favoritesInfo
-
-//           next();
-//         })
-//         .catch((err) => console.log('error', err));
-
-//     } catch (err) {
-//       next(err);
-//     }
-//   }
-//   else {
-//     next();
-//   }
-// }
-
 module.exports = favoriteController;
 
 
